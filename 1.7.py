5af42a7800f14268a6dc51f9e0b1b0d3569aba67
Francesco Leuzzi <90636442+frame88@users.noreply.github.com>
2022-02-28 16:33:35 +0100

    funzione LEN, restituisce la lunghezza della stringa in input

---
+++ 1.7.py
@@ -0,0 +1,7 @@
+word_1 = input('inserisci una parola: ')
+word_2 = input('inserisci un\'altra parola: ')
+
+c = len(word_1) + len(word_2)
+
+
+print('La lunghezza complessiva delle due stringhe è ', c)
